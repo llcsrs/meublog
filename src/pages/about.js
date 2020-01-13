bfbbdf2d09f406d6705e31ed73cbba2f5da7a9d9
llcsrs <lcsrss@gmail.com>
2020-01-13 14:33:18 -0300

    git púll

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,27 +1,12 @@
-import React  from 'react';
-import { Link } from "gatsby"
+import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 
-
-const AboutPage = () =>(
-   <Layout>   
-       <SEO title ="About" />
-        <h1>About </h1>
-        
-        <ul>
-            <li>
-                    <Link to="/" activeStyle={{ color: "red" }}>
-                    Home
-                </Link>
-            </li>
-            <li>
-                <Link to="/about" activeStyle={{ color: "red" }}>
-                    About (Gatsby Link)
-                </Link>
-            </li>
-    </ul>
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About" />
+    <h1>About Page</h1>
   </Layout>
-
 )
+
 export default AboutPage
