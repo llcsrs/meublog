c498a85c7c64649b7f639b8f28b28253830418f5
llcsrs <lcsrss@gmail.com>
2020-01-14 14:27:16 -0300

    git push

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,21 +1,18 @@
-import { useStaticQuery, graphql } from "gatsby"
-
 import Avatar from "../Avatar"
 import * as S from "./styled"
 
 const Profile = () => {
-
   return (
-  <S.ProfileWrapper>
-  <S.ProfileLink>
-    <Avatar />
-    <S.ProfileAuthor>
-      {title}
-      <S.ProfilePosition>{position}</S.ProfilePosition>
-    </S.ProfileAuthor>
-  </S.ProfileLink>
-  <S.ProfileDescription>{description}</S.ProfileDescription>
-</S.ProfileWrapper>
+    <S.ProfileWrapper>
+    <S.ProfileLink>
+      <Avatar />
+      <S.ProfileAuthor>
+        {title}
+        <S.ProfilePosition>{position}</S.ProfilePosition>
+      </S.ProfileAuthor>
+    </S.ProfileLink>
+    <S.ProfileDescription>{description}</S.ProfileDescription>
+  </S.ProfileWrapper>
 )
 }
     export default Profile
