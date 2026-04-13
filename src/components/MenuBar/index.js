3d8e8287648e51c62826aa39fc2d82167ab24c4b
lcsrss <31260451+llcsrs@users.noreply.github.com>
2026-04-13 16:51:39 -0300

    dgjhdfjhdfj

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react"
 
-import { Home } from "styled-icons/boxicons-solid/Home"
-import { SearchAlt2 as Search } from "styled-icons/boxicons-regular/SearchAlt2"
-import { UpArrowAlt as Arrow } from "styled-icons/boxicons-regular/UpArrowAlt"
-import { Lightbulb as Light } from "styled-icons/fa-regular/Lightbulb"
-import { Grid } from "styled-icons/boxicons-solid/Grid"
-import { ThList as List } from "styled-icons/typicons/ThList"
+import { Home, Grid } from "styled-icons/boxicons-solid"
+import { SearchAlt2 as Search, UpArrowAlt as Arrow } from "styled-icons/boxicons-regular"
+import { Lightbulb as Light } from "styled-icons/fa-regular"
+import { ThList as List } from "styled-icons/typicons"
 
 import getThemeColor from "../../utils/getThemeColor"
 
